80d6bfbec77132ca8d54d48c11e2289b5a7b01a2
Carsten Dreesbach <carsten.dreesbach@opower.com>
2012-12-14 23:38:34 -0500

    Initial comit of dotfiles
    Inspired by http://mirnazim.org/writings/vim-plugins-i-use/

---
+++ link_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# ack settings
+ln -s ~/dotfiles/ack/ackrc              ~/.ackrc
+# bash settings
+ln -s ~/dotfiles/bash/profile           ~/.profile
+ln -s ~/dotfiles/bash/bash_profile      ~/.bash_profile
+ln -s ~/dotfiles/bash/bashrc            ~/.bashrc
+# git settings
+ln -s ~/dotfiles/git/gitconfig          ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore_global   ~/.gitignore_global
+# screen settings
+ln -s ~/dotfiles/screen/screenrc        ~/.screenrc
+# vim settings
+ln -s ~/dotfiles/vim                    ~/.vim
+ln -s ~/dotfiles/vim/vimrc              ~/.vimrc
+# zsh settings
+ln -s ~/dotfiles/zsh/zshrc              ~/.zshrc
